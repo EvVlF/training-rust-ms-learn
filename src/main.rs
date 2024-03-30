a886dfedd8ffeb945627c51314011a9300c0aec1
EvVlF <ev.vl.fedorov@gmail.com>
2024-03-30 22:05:58 +0400

    change index value

--- src/main.rs
+++ src/main.rs
@@ -3,4 +3,8 @@ fn main() {
     let mut index_vec = vec![15, 3, 46];
     let three = index_vec[1];
     println!("Vector: {:?}, three = {}", index_vec, three);
+
+    // Add 5 to the value at index 1, which is 5 + 3 = 8
+    index_vec[1] = index_vec[1] + 5;
+    println!("Vector: {:?}", index_vec);
 }
