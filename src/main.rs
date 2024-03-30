a81014575e71a2794c912cae4b11e158d0631e4d
EvVlF <ev.vl.fedorov@gmail.com>
2024-03-30 21:56:28 +0400

    vectors

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,9 @@
 fn main() {
-// Declare array, initialize all values, compiler infers length = 7
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// Declare vector, initialize with three values
+    let three_nums = vec![15, 3, 46];
+    println!("Initial vector: {:?}", three_nums);
 
-// Declare array, initialize all values to 0, length = 5
-    let bytes = [0; 5];
-
-// Days of the week
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-
-// Get the first day of the week
-    let first = days[0];
-
-// Get the second day of the week
-    let second = days[1];
-
-// Get the seventh day of the week using the wrong index - should be 6
-    let seventh = days[7];
+// Declare vector, value = "0", length = 5
+    let zeroes = vec![0; 5];
+    println!("Zeroes: {:?}", zeroes);
 }
