559e31ea4c035b0e7ede030c1bbcd2e1363d4589
EvVlF <ev.vl.fedorov@gmail.com>
2024-03-30 15:35:19 +0400

    call a function

--- src/main.rs
+++ src/main.rs
@@ -23,12 +23,22 @@ fn car_factory(color: String, transmission: Transmission, convertible: bool) -> 
     // Use the values of the input arguments
     // All new cars always have zero mileage
     Car {
-        color: String::from(
-            "Red"),
-        transmission: Transmission::Automatic,
-        convertible: true,
+        color,
+        transmission,
+        convertible,
         mileage: 0,
     }
 }
 
-fn main() {}
+fn main() {
+    // We have orders for three new cars!
+    // We'll declare a mutable car variable and reuse it for all the cars
+    let mut car = car_factory(String::from("Red"), Transmission::Manual, false);
+    println!("Car 1 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Silver"), Transmission::Automatic, true);
+    println!("Car 2 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+
+    car = car_factory(String::from("Yellow"), Transmission::SemiAuto, false);
+    println!("Car 3 = {}, {:?} transmission, convertible: {}, mileage: {}", car.color, car.transmission, car.convertible, car.mileage);
+}
