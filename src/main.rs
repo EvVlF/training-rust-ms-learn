ef4cc805c8b8d1fb49c9702d11737c5a9372e1b7
EvVlF <ev.vl.fedorov@gmail.com>
2024-03-31 17:02:02 +0400

    exercise: work with compound types create the car_quality function

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ struct Car {
     color: String,
     motor: Transmission,
     roof: bool,
-    tup: (Age, u32),
+    age: (Age, u32),
 }
 
 #[derive(PartialEq, Debug)]
@@ -13,5 +13,19 @@ enum Transmission { Manual, SemiAuto, Automatic }
 
 #[derive(PartialEq, Debug)]
 enum Age { New, Used }
+
+// Get the car quality by testing the value of the input argument
+// - miles (u32)
+// Create a tuple for the car quality with the Age ("New" or "Used") and mileage
+// Return a tuple with the arrow `->` syntax
+fn car_quality(miles: u32) -> (Age, u32) {
+
+    // Declare and initialize the return tuple value
+    // For a new car, set the miles to 0
+    let quality: (Age, u32) = (Age::New, miles);
+
+    // Return the completed tuple to the caller
+    quality
+}
 
 fn main() {}
