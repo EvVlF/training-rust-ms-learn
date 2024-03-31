e7766f652f617e4c1e353fb278f154fc856387f7
EvVlF <ev.vl.fedorov@gmail.com>
2024-03-31 13:42:59 +0400

    exercise: work with compound types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-fn main() {
-// Declare vector, initialize with three values
-    let mut index_vec = vec![15, 3, 46];
+#[derive(PartialEq, Debug)]
+// Declare Car struct to describe vehicle with four named fields
+struct Car {
+    color: String,
+    motor: Transmission,
+    roof: bool,
+    mileage: u32, // todo!("Move `mileage: u32` field into `age` field - a tuple with two fields: an `Age` enum, u32");
+}
 
-    // Access vector with out-of-bounds index
-    let beyond = index_vec[10];
-    println!("{}", beyond);
+#[derive(PartialEq, Debug)]
+// Declare enum for Car transmission type
+enum Transmission { Manual, SemiAuto, Automatic }
+
+fn main() {
+
 }
