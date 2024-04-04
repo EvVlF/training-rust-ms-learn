8a411dddcf7a1cbb04712250719c4e17a25ee84a
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-04 20:14:21 +0400

    exercise: work with compound types call a function to fulfill the car orders

--- src/main.rs
+++ src/main.rs
@@ -60,4 +60,20 @@ fn main() {
         age: (Age::New, 2),
     };
     let mut engine: Transmission = Manual;
+
+    // Order 3 cars, one car for each type of transmission
+
+    // Car order #1: New, Manual, Hard top
+    car = car_factory(String::from(&format!("{:?}", colors[2])), engine, true, 0);
+    println!("Car order 1: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #2: Used, Semi-automatic, Convertible
+    engine = Transmission::SemiAuto;
+    car = car_factory(String::from(&format!("{:?}", colors[3])), engine, false, 100);
+    println!("Car order 2: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
+
+    // Car order #3: Used, Automatic, Hard top
+    engine = Transmission::Automatic;
+    car = car_factory(String::from(&format!("{:?}", colors[1])), engine, true, 200);
+    println!("Car order 3: {:?}, Hard top = {}, {:?}, {}, {} miles", car.age.0, car.roof, car.motor, car.color, car.age.1);
 }
