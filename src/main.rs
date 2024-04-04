d231b80bb2acd871450f02bc81ec8850a645bdfe
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-04 20:41:38 +0400

    use if/else conditions combine multiple test conditions with "else if"

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 fn main() {
-    let formal = true;
-    let greeting = if formal { // if used here as an expression
-        "Good day to you."     // return a String
+    let num = 500; // num variable can be set at some point in the program
+    let out_of_range: bool;
+    if num < 0 {
+        out_of_range = true;
+    } else if num == 0 {
+        out_of_range = true;
+    } else if num > 512 {
+        out_of_range = true;
     } else {
-        "Hey!"                 // return a String
-    };
-    println!("{}", greeting)   // prints "Good day to you."
+        out_of_range = false;
+    }
+    print!("{}", out_of_range);
 }
