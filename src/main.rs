771558bc81b3bbb960051bdfcd8ca6c69fc2cc70
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-07 00:37:51 +0400

    use for, while, and loop expressions loop a while

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,7 @@
 fn main() {
     let mut counter = 1;
-// stop_loop is set when loop stops
-    let stop_loop = loop {
-        counter *= 2;
-        if counter > 100 {
-            // Stop loop, return counter value
-            break counter;
-        }
-    };
-// Loop should break when counter = 128
-    println!("Break the loop at counter = {}.", stop_loop);
+    while counter < 5 {
+        println!("We loop a while...");
+        counter = counter + 1;
+    }
 }
