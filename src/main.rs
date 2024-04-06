e275a13400085fbdc33ec152b75b1a66e36db0e8
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-06 19:37:25 +0400

    HashMap

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,20 @@
-use crate::Age::{New, Used};
-
-#[derive(PartialEq, Debug)]
-// Declare Car struct to describe vehicle with four named fields
-struct Car {
-    color: String,
-    motor: Transmission,
-    roof: bool,
-    age: (Age, u32),
-}
-
-#[derive(PartialEq, Debug)]
-// Declare enum for Car transmission type
-enum Transmission { Manual, SemiAuto, Automatic }
-
-#[derive(PartialEq, Debug)]
-// Declare enum for Car age
-enum Age { New, Used }
-
-//////////////////////////////////////////////////
-
-// Get the car quality by testing the value of the input argument
-// - miles (u32)
-// Return tuple with car age ("New" or "Used") and mileage
-fn car_quality(miles: u32) -> (Age, u32) {
-    if miles > 0 {
-        return (Used, miles);
-    }
-    (New, 0)
-}
-
-//////////////////////////////////////////////////
-
-// Build a new "Car" using the values of four input arguments
-// - color (String)
-// - motor (Transmission enum)
-// - roof (boolean, true if the car has a hard top roof)
-// - miles (u32)
-// Call the car_quality(miles) function to get the car age
-// Return an instance of a "Car" struct with the arrow `->` syntax
-fn car_factory(color: String, motor: Transmission, roof: bool, miles: u32) -> Car {
-
-    // Show details about car order
-    // - Check if order is for Used or New car, then check the roof type
-    // - Print details for New or Used car based on roof type
-    if car_quality(miles).0 == New {
-        if roof {
-            // Call the `println!` macro to show the car order details
-            println!("Prepare a used car: {:?}, {}, Hard top, {} miles\n", motor, color, miles);
-        } else {
-            println!("Preparing a used car: {:?}, {}, Convertible, {} miles", motor, color, miles);
-        }
-    } else {
-        if roof {
-            println!("Building a new car: {:?}, {}, Hard top, {} miles", motor, color, miles);
-        } else {
-            println!("Building a new car: {:?}, {}, Convertible, {} miles", motor, color, miles);
-        }
-    }
-
-    // Create a new "Car" instance as requested
-    // - Bind first three fields to values of input arguments
-    // - Bind "age" to tuple returned from car_quality(miles)
-    Car {
-        color: color,
-        motor: motor,
-        roof: roof,
-        age: car_quality(miles),
-    }
-}
-
 fn main() {
-    // Car order #1: New, Manual, Hard top
-    car_factory(String::from("Orange"), Transmission::Manual, true, 0);
+    use std::collections::HashMap;
+    let mut reviews: HashMap<String, String> = HashMap::new();
 
-    // Car order #2: Used, Semi-automatic, Convertible
-    car_factory(String::from("Red"), Transmission::SemiAuto, false, 565);
+    reviews.insert(String::from("Ancient Roman History"), String::from("Very accurate."));
+    reviews.insert(String::from("Cooking with Rhubarb"), String::from("Sweet recipes."));
+    reviews.insert(String::from("Programming in Rust"), String::from("Great examples."));
 
-    // Car order #3: Used, Automatic, Hard top
-    car_factory(String::from("White"), Transmission::Automatic, true, 3000);
+    // Look for a specific review
+    let book: &str = "Programming in Rust";
+    println!("\nReview for \'{}\': {:?}", book, reviews.get(book));
+
+    // Remove book review
+    let obsolete: &str = "Ancient Roman History";
+    println!("\n'{}\' removed.", obsolete);
+    reviews.remove(obsolete);
+
+    // Confirm book review removed
+    println!("\nReview for \'{}\': {:?}", obsolete, reviews.get(obsolete));
 }
