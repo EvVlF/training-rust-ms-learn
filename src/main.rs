9054cdebe5385830f8d3bf4fff4c78f7cc7462b1
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-07 00:35:35 +0400

    use for, while, and loop expressions just keep looping with break and also return a value at the break point.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,13 @@
 fn main() {
-    loop {
-        // Keep printing, printing, printing...
-        println!("We loop forever!");
-        // On the other hand, maybe we should stop!
-        break;
-    }
+    let mut counter = 1;
+// stop_loop is set when loop stops
+    let stop_loop = loop {
+        counter *= 2;
+        if counter > 100 {
+            // Stop loop, return counter value
+            break counter;
+        }
+    };
+// Loop should break when counter = 128
+    println!("Break the loop at counter = {}.", stop_loop);
 }
