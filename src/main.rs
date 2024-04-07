40333db2accee65be33d5ad4aaf16ee2c56f8adc
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-07 12:51:41 +0400

    use for, while, and loop expressions loop for these values with iter()

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 fn main() {
     let big_birds = ["ostrich", "peacock", "stork"];
-    for bird in big_birds{}
+    for bird in big_birds.iter() {
+        println!("The {} is a big bird.", bird);
+    }
 }
