1db2b7e79d1ca2e1a2d24262baea725bfa59ba27
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-09 21:10:56 +0400

    exercise - use the Option type to deal with absence

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,44 @@
-fn main() {
-    let a = Some("value");
-    assert_eq!(a.expect("fruits are healthy"), "value");
+struct Person {
+    first: String,
+    middle: Option<String>,
+    last: String,
+}
 
-    let b: Option<&str> = None;
-    b.expect("fruits are healthy"); // panics with `fruits are healthy`
+fn build_full_name(person: &Person) -> String {
+    let mut full_name = String::new();
+    full_name.push_str(&person.first);
+    full_name.push_str(" ");
+
+    // TODO: Implement the part of this function that handles the person's middle name.
+
+    if let Some(middle) = &person.middle {
+        full_name.push_str(middle);
+        full_name.push_str(" ");
+    }
+
+    full_name.push_str(&person.last);
+    full_name
+}
+
+fn main() {
+    let john = Person {
+        first: String::from("James"),
+        middle: Some(String::from("Oliver")),
+        last: String::from("Smith"),
+    };
+    assert_eq!(build_full_name(&john), "James Oliver Smith");
+
+    let alice = Person {
+        first: String::from("Alice"),
+        middle: None,
+        last: String::from("Stevens"),
+    };
+    assert_eq!(build_full_name(&alice), "Alice Stevens");
+
+    let bob = Person {
+        first: String::from("Robert"),
+        middle: Some(String::from("Murdock")),
+        last: String::from("Jones"),
+    };
+    assert_eq!(build_full_name(&bob), "Robert Murdock Jones");
 }
