9d69221578bd0b708db372183af6f268e5643464
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-09 20:11:37 +0400

    handle errors in Rust if let expression

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 fn main() {
     let a_number: Option<u8> = Some(7);
-    match a_number {
-        Some(7) => println!("That's my lucky number!"),
-        _ => {}
+    if let Some(7) = a_number {
+        println!("That's my lucky number!");
     }
 }
