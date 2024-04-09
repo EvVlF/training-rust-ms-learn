900a6e1749027a848d2abad1af80153b25c8a684
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-09 20:03:43 +0400

    handle errors in Rust the if let expression

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
 fn main() {
-    let fruits = vec!["banana", "apple", "coconut", "orange", "strawberry"];
-    for &index in [0, 2, 99].iter() {
-        match fruits.get(index) {
-            Some(&"coconut") => println!("Coconuts are awesome!!!"),
-            Some(fruit_name) => println!("It's a delicious {}!", fruit_name),
-            None => println!("There is no fruit! :("),
-        }
+    let a_number: Option<u8> = Some(7);
+    match a_number {
+        Some(7) => println!("That's my lucky number!"),
+        _ => {}
     }
 }
