142f71c746749102043c5f3c877b7866dc63c07e
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-14 05:58:01 +0400

    borrowing. mutate borrowed values

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-fn print_greeting(message: &String) {
-    println!("Greeting: {}", message);
+fn main() {
+    let mut greeting = String::from("hello");
+    change(&mut greeting);
 }
 
-fn main() {
-    let greeting = String::from("Hello");
-    print_greeting(&greeting); // `print_greeting` takes a `&String` not an owned `String` so we borrow `greeting` with `&`
-    print_greeting(&greeting); // Since `greeting` didn't move into `print_greeting` we can use it again
+fn change(text: &mut String) {
+    text.push_str(", world");
 }
