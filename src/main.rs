33f5e3b1ed9cad9bf3745ffe14dfefc9d2c942a5
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-14 13:28:28 +0400

    annotating lifetimes in types

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,10 @@
-fn main() {
-    let magic1 = String::from("abracadabra!");
-    let result;
-    {
-        let magic2 = String::from("shazam!");
-        result = longest_word(&magic1, &magic2);
-    }
-    println!("The longest magic word is {}", result);
-}
+#[derive(Debug)]
+struct Highlight<'document>(&'document str);
 
-fn longest_word<'a>(x: &'a String, y: &'a String) -> &'a String {
-    if x.len() > y.len() {
-        x
-    } else {
-        y
-    }
+fn main() {
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    println!("{:?}", fox);
+    println!("{:?}", dog);
 }
