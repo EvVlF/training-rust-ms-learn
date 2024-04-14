14a1d4682758354d0d61d51000df7428a5dca9bc
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-14 06:05:18 +0400

    validate references by using lifetimes '

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let x;
-    {
-        let y = 42;
-        x = &y; // We store a reference to `y` in `x` but `y` is about to be dropped.
-    }
-    println!("x: {}", x); // `x` refers to `y` but `y has been dropped!
+    let x;                // ---------+-- 'a
+    {                     //          |
+        let y = 42;       // -+-- 'b  |
+        x = &y;           //  |       |
+    }                     // -+       |
+    println!("x: {}", x); //          |
 }
