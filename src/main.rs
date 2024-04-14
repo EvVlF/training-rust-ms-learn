cc96a77126bf246f54ac54cfc8096961eafe01b2
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-14 05:46:36 +0400

    copying types that don't implement Copy

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-fn main() {}
+fn process(s: String) {}
 
-fn process(input: u32) {}
-
-fn caller() {
-    let n = 1u32;
-    process(n); // Ownership of the number in `n` copied into `process`
-    process(n); // `n` can be used again because it wasn't moved, it was copied.
+fn main() {
+    let s = String::from("Hello, world!");
+    process(s.clone()); // Passing another value, cloned from `s`.
+    process(s); // s was never moved and so it can still be used.
 }
