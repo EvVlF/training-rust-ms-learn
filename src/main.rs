cac02a40ab1b7649c8461397e34888b4e8a4ad43
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-18 20:40:58 +0400

    generic data types, not correct - different types x y

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,5 @@ struct Point<T> {
 }
 
 fn main() {
-    let boolean = Point { x: true, y: false };
-    let integer = Point { x: 1, y: 9 };
-    let float = Point { x: 1.7, y: 4.3 };
-    let string_slice = Point { x: "high", y: "low" };
+    let wont_work = Point { x: 25, y: true };
 }
