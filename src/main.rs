0765bed3c5f6a75c0df76d1719f89148298f1271
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-21 12:33:20 +0400

    use the derive trait

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
-trait Area {
-    fn area(&self) -> f64;
-}
-
-struct Circle {
-    radius: f64,
-}
-
-struct Rectangle {
-    width: f64,
-    height: f64,
-}
-
-impl Area for Circle {
-    fn area(&self) -> f64 {
-        use std::f64::consts::PI;
-        PI * self.radius.powf(2.0)
-    }
-}
-
-impl Area for Rectangle {
-    fn area(&self) -> f64 {
-        self.width * self.height
-    }
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
-    let circle = Circle { radius: 5.0 };
-    let rectangle = Rectangle {
-        width: 10.0,
-        height: 20.0,
-    };
+    let p1 = Point { x: 1, y: 2 };
+    let p2 = Point { x: 4, y: -3 };
 
-    println!("Circle area: {}", circle.area());
-    println!("Rectangle area: {}", rectangle.area());
+    if p1 == p2 { // can't compare two Point values!
+        println!("equal!");
+    } else {
+        println!("not equal!");
+    }
+
+    println!("{}", p1); // can't print using the '{}' format specifier!
+    println!("{:?}", p1); //  can't print using the '{:?}' format specifier!
+
 }
