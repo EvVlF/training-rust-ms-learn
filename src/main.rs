5cf3fe55e8eab471382d501b42f1b23aae877222
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-23 19:50:42 +0400

    use trait bounds and generic functions; function that accepts any type that implements the AsJson trait

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,10 @@ trait AsJson {
     fn as_json(&self) -> String;
 }
 
+fn send_data_as_json(value: &impl AsJson) {
+    println!("Sending JSON data to server...");
+    println!("-> {}", value.as_json());
+    println!("Done!\n");
+}
+
 fn main() {}
