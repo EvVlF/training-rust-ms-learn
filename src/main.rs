e72226181894e48704208f14843a20eae922db44
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-23 20:04:25 +0400

    use trait bounds and generic functions; impl AsJson

--- src/main.rs
+++ src/main.rs
@@ -8,4 +8,34 @@ fn send_data_as_json<T: AsJson>(value: &T) {
     println!("Done!\n");
 }
 
+struct Person {
+    name: String,
+    age: u8,
+    favorite_fruit: String,
+}
+
+struct Dog {
+    name: String,
+    color: String,
+    likes_petting: bool,
+}
+
+impl AsJson for Person {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{ "type": "person", "name": "{}", "age": {}, "favoriteFruit": "{}" }}"#,
+            self.name, self.age, self.favorite_fruit
+        )
+    }
+}
+
+impl AsJson for Dog {
+    fn as_json(&self) -> String {
+        format!(
+            r#"{{ "type": "dog", "name": "{}", "color": "{}", "likesPetting": {} }}"#,
+            self.name, self.color, self.likes_petting
+        )
+    }
+}
+
 fn main() {}
