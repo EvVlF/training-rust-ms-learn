521ea5592e993df1c860480cef3885b6c00c97db
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-24 21:18:52 +0400

    exercise - implement a generic type; initialize

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,19 @@
-#[derive(Debug)]
-struct Counter {
-    length: usize,
-    count: usize,
+struct Container {
+    value: u32,
 }
 
-impl Counter {
-    fn new(length: usize) -> Counter {
-        Counter {
-            count: 0,
-            length,
-        }
-    }
-}
-
-impl Iterator for Counter {
-    type Item = usize;
-
-    fn next(&mut self) -> Option<Self::Item> {
-
-        self.count += 1;
-        if self.count <= self.length {
-            Some(self.count)
-        } else {
-            None
-        }
+impl Container {
+    pub fn new(value: u32) -> Self {
+        Container { value }
     }
 }
 
 fn main() {
-    let sum_until_10: usize = Counter::new(10).sum();
-    assert_eq!(sum_until_10, 55);
-
-    let powers_of_2: Vec<usize> = Counter::new(8).map(|n| 2usize.pow(n as u32)).collect();
-    assert_eq!(powers_of_2, vec![2, 4, 8, 16, 32, 64, 128, 256]);
+    assert_eq!(Container::new(42).value, 42);
+    assert_eq!(Container::new(3.14).value, 3.14);
+    assert_eq!(Container::new("Foo").value, "Foo");
+    assert_eq!(Container::new(String::from("Bar")).value, String::from("Bar"));
+    assert_eq!(Container::new(true).value, true);
+    assert_eq!(Container::new(-12).value, -12);
+    assert_eq!(Container::new(Some("text")).value, Some("text"));
 }
