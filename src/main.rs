84388461b55f42a39c44409c248a1f4e5c8ba1e3
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-24 20:41:52 +0400

    implement own iterator

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-trait Iterator {
-    type Item;
-    fn next(&mut self) -> Option<Self::Item>;
+#[derive(Debug)]
+struct Counter {
+    length: usize,
+    count: usize,
+}
+
+impl Counter {
+    fn new(length: usize) -> Counter {
+        Counter {
+            count: 0,
+            length,
+        }
+    }
 }
 
 fn main() {
