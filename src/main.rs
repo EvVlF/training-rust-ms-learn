a50427aa3e3c88cae09311a5f7006418fbf0bb6e
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-29 13:11:58 +0400

    add third-party crates to your project; add 'regex' dependency; cargo.toml, crates.io; cargo build

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,3 @@
-mod authentication;
-
 fn main() {
-    let mut user = authentication::User::new("jeremy", "super-secret");
 
-    println!("The username is: {}", user.get_username());
-    user.set_password("even-more-secret");
 }
