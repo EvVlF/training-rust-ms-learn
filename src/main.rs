db7d9e6cbe719ae345eff7ba96ee1c4dfd52d792
EvVlF <ev.vl.fedorov@gmail.com>
2024-04-29 13:36:01 +0400

    exercise - modules

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,26 @@
-mod car_factory {
-    pub fn build_car() {
-        println!("Honk honk!");
+mod text_processing {
+
+    mod letters {
+        fn count_letters(text: &str) -> usize {
+            text.chars().filter(|ref c| c.is_alphabetic()).count()
+        }
+    }
+
+    mod numbers {
+        fn count_numbers(text: &str) -> usize {
+            text.chars().filter(|ref c| c.is_numeric()).count()
+        }
     }
 }
 
+fn count_letters_and_numbers(text: &str) -> (usize, usize) {
+    let number_of_letters = ???;
+    let number_of_numbers = ???;
+    (number_of_letters, number_of_numbers)
+}
+
 fn main() {
-    car_factory::build_car();
+    assert_eq!(count_letters_and_numbers("221B Baker Street"), (12, 3));
+    assert_eq!(count_letters_and_numbers("711 Maple Street"), (11, 3));
+    assert_eq!(count_letters_and_numbers("4 Privet Drive"), (11, 1));
 }
