79dd92bc64da7a03d62709bb7cc77252f0aa0fee
EvVlF <ev.vl.fedorov@gmail.com>
2024-05-06 16:37:22 +0400

    write unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,12 @@
-mod text_processing {
+fn main() {}
 
-    pub mod letters {
-        pub fn count_letters(text: &str) -> usize {
-            text.chars().filter(|ref c| c.is_alphabetic()).count()
-        }
-    }
-
-    pub mod numbers {
-        pub fn count_numbers(text: &str) -> usize {
-            text.chars().filter(|ref c| c.is_numeric()).count()
-        }
-    }
+fn add(a: i32, b: i32) -> i32 {
+    a + b
 }
 
-fn count_letters_and_numbers(text: &str) -> (usize, usize) {
-    let number_of_letters = text_processing::letters::count_letters(&text);
-    let number_of_numbers = text_processing::numbers::count_numbers(&text);
-    (number_of_letters, number_of_numbers)
-}
-
-fn main() {
-    assert_eq!(count_letters_and_numbers("221B Baker Street"), (12, 3));
-    assert_eq!(count_letters_and_numbers("711 Maple Street"), (11, 3));
-    assert_eq!(count_letters_and_numbers("4 Privet Drive"), (11, 1));
+#[test]
+fn add_works() {
+    assert_eq!(add(1, 2), 3);
+    assert_eq!(add(10, 12), 22);
+    assert_eq!(add(5, -2), 3);
 }
