be9c6ee3488054d6269cca7abd58d6a2f17a276f
EvVlF <ev.vl.fedorov@gmail.com>
2024-05-06 17:04:31 +0400

    exercise - write unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,20 @@
 fn main() {}
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+pub fn is_even(num: i32) -> bool {
+    num % 2 == 0
 }
 
 #[cfg(test)]
-mod add_function_tests {
+mod tests {
     use super::*;
 
     #[test]
-    fn add_works() {
-        assert_eq!(add(1, 2), 3);
-        assert_eq!(add(10, 12), 22);
-        assert_eq!(add(5, -2), 3);
+    fn is_true_when_even() {
+        assert!();
     }
 
     #[test]
-    #[should_panic]
-    fn add_fails() {
-        assert_eq!(add(2, 2), 7);
+    fn is_false_when_odd() {
+        assert!();
     }
-
-    #[test]
-    #[ignore]
-    fn add_negatives() {
-        assert_eq!(add(-2, -2), -4)
-    }
-}   
+}
